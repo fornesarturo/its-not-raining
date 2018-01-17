4abdc3cf1497feb53b5ac75ef1ab22823880866e
fornesarturo <fornesarturo@gmail.com>
2018-01-16 23:07:39 -0600

    better physics

--- html/index.js
+++ html/index.js
@@ -2,63 +2,84 @@
 const SPEED = 7;
 const GRAVITY = 1;
 const JUMP = 15;
+const WALL_GRAB = 2;
 
 // Sprites.
 var mySprite;
-// Structures.
-var floor, wall;
 
-var walled;
+// Structures.
+var ground, walls;
+
+var collidingWall;
+
+var walled, grounded, direction, waitForMovement;
 
 function setup() {
     createCanvas(800, 600);
-    mySprite = createSprite(400, 200, 50, 50);
-    floor = createSprite(400, 600, 800, 100);
-    wall = createSprite(0, 300, 200, 500);
+    player = createSprite(400, 200, 50, 50);
+    ground = createSprite(400, 600, 800, 100);
+    walls = Group();
+
+    let wallA = createSprite(50, 300, 100, 500);
+    let wallB = createSprite(350, 300, 100, 500);
+    walls.add(wallA);
+    walls.add(wallB);
     walled = false;
+    grounded = false;
+    waitForMovement = false;
+
+    player.velocity.x = 0;
 }
 
 function draw() {
     background(0, 0, 0);
     walled = false;
-    mySprite.velocity.x = 0;
-    mySprite.velocity.y += GRAVITY;
 
     // Basic Movement.
-    if (keyDown("left"))
-        mySprite.velocity.x = -SPEED;
-    if (keyDown("right"))
-        mySprite.velocity.x = SPEED;
-
-    // Floor and Wall.
-    if (mySprite.collide(floor)) {
-        mySprite.velocity.y = 0;
-        if (keyWentDown("space"))
-            mySprite.velocity.y = -JUMP;
+    if (keyDown("left")) {
+        if (!waitForMovement)
+            player.velocity.x = -SPEED;
+    }
+    if (keyDown("right")) {
+        if (!waitForMovement)
+            player.velocity.x = SPEED;
     }
 
-    if (mySprite.collide(wall)) {
-        let direction = wall.position.x - mySprite.position.x;
-        mySprite.velocity.x = 0;
-        if (direction < 0) {
-            if (keyDown("left")) {
-                mySprite.velocity.y += GRAVITY/2;
-                // Wall hog to the left.
-                if (keyWentDown("space")) {
-                    mySprite.velocity.y = -JUMP * 2;
-                    mySprite.velocity.x = -JUMP * 4;
-                }
+    // Stop Movement.
+    if (keyWentUp("left") || keyWentDown("right")) {
+        player.velocity.x = 0;
+    }
+    
+    player.collide(ground, function(a, b) {
+        grounded = true;
+    });
+
+    if (!grounded) {
+        player.velocity.y += GRAVITY;
+    }
+
+    player.collide(walls, function(sprite, target) {
+        walled = true;
+        player.velocity.y = WALL_GRAB;
+        direction = target.position.x - sprite.position.x;
+    });
+
+    if (keyWentDown("space")) {
+        if(walled && !grounded) {
+            waitForMovement = true;
+            if (direction < 0) {
+                player.velocity.y = -JUMP;
+                player.velocity.x = JUMP;
+            } else {
+                player.velocity.y = -JUMP; 
+                player.velocity.x = -JUMP;
             }
-        } else {
-            if (keyDown("right")) {
-                mySprite.velocity.y += GRAVITY / 2;
-                // Wall hog to the right.
-                if (keyWentDown("space")) {
-                    mySprite.velocity.y = -JUMP * 2;
-                    mySprite.velocity.x = -JUMP * 4;
-                }
-            }
-            
+            setTimeout(function () {
+                waitForMovement = false;
+            }, 100);
+        } else if (grounded) {
+            player.velocity.y = -JUMP;
+            grounded = false;
         }
     }
 
