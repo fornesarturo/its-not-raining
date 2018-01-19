0434bcf5f36307f76da164a4bf71ca8fd171fdca
fornesarturo <fornesarturo@gmail.com>
2018-01-19 12:22:24 -0600

    Demo for getting levels through HTTP POST
    In this commit:
    * Added body-parser to parse application/json as JSON.
    * Answers a JSON-ified version of a level given a certain level id.
    
    To-Do:
    * Migrate to DB.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 1337;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 var gameRouter = express.Router();
 // var apiRouter = express.Router();
@@ -12,8 +16,32 @@ app.use(express.static('html'))
 app.use('/', gameRouter);
 
 gameRouter.route('/')
-	.get(function (req, res, next){
+	.get(function (req, res, next) {
 		res.status(200).sendFile(path.join(__dirname + '/html/index.html'));
+	})
+	.post(function (req, res, next) {
+		if (req.body["id"] == 1) {
+			let level = {
+				"player": [
+					[300, 200]
+				],
+				"structures": [
+					[50, 300, 100, 500],
+					[400, 300, 100, 500],
+					[750, 300, 100, 500],
+					[400, 600, 1800, 100]
+				],
+				"obstacles": [
+					[500, 500, 50, 50]
+				],
+				"end": [
+					[1200, 500, 50, 50]
+				]
+			}
+
+			res.json(level);
+		} else
+			res.json(req.body);
 	});
 
 app.listen(PORT, function () {
