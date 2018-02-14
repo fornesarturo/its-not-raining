925e85dfe4a2b6e2cb84e15c9e377e1c3f13bdbc
geraJuarez <geratrex781@gmail.com>
2018-02-13 19:38:48 -0600

    Added rain effect to menu
    To do:
    * Give credit for the rain effect to the user of github.

--- html/scenes/menu.js
+++ html/scenes/menu.js
@@ -1,47 +1,59 @@
 function Menu() {
+    var drops = [];
     this.setup = () => {
-        background(0);
-        textAlign(CENTER, CENTER);
-        rectMode(CENTER);
-        noLoop();
+        for (var i = 0; i < 100; i++) {
+            drops[i] = new Drop();
+        }
     }
 
     this.draw = () => {
-        translate(WIDTH / 2, 0);
-        strokeWeight(6);
-        stroke(255);
-        fill(0, 0);
-        rect(0, HEIGHT * 0.15, 600, 130);
+        background(0);
+        textAlign(CENTER, CENTER);
+        rectMode(CENTER);
 
-        fill(255);
-        noStroke();
-        textFont('Arial');
-
-        textSize(80);
-        text("It's Not Raining", 0, HEIGHT * 0.15);
-
-        textSize(50);
-        text("MENU", 0, HEIGHT * 0.4);
-
-        textSize(40);
-        text("> Press '1' to play", 0, 650);
-
-        textSize(40);
-        text("> Press '2' for leaderboard", 0, 750);
+        drawText();
+        
+        // Draw rain
+        for (var i = 0; i < drops.length; i++) {
+            drops[i].fall();
+            drops[i].show();
+        }
     }
 
     this.keyPressed = () => {
         switch(key) {   
             case '1':
                 console.log("Game");
-                loop();
                 this.sceneManager.showScene(Game);
                 break;
             case '2':
                 console.log("Leaderboard");
-                loop();
                 this.sceneManager.showScene(Leaderboard); 
                 break;   
         }
+    }
+
+    function drawText() {
+        translate(WIDTH / 2, 0);
+        strokeWeight(6);
+        stroke(255);
+        fill(0, 0);
+        rect(0, HEIGHT * 0.15, 600, 130);
+    
+        fill(255);
+        noStroke();
+        textFont('Arial');
+    
+        textSize(80);
+        text("It's Not Raining", 0, HEIGHT * 0.15);
+    
+        textSize(50);
+        text("MENU", 0, HEIGHT * 0.4);
+    
+        textSize(40);
+        text("> Press '1' to play", 0, 650);
+    
+        textSize(40);
+        text("> Press '2' for leaderboard", 0, 750);
     }
 }
