8e74a8f6b7de226fb4fdb314b7b09fcef99c90d9
geraJuarez <geratrex781@gmail.com>
2018-02-13 19:03:18 -0600

    Change menu style

--- html/scenes/menu.js
+++ html/scenes/menu.js
@@ -1,29 +1,45 @@
 function Menu() {
     this.setup = () => {
-
+        background(0);
+        textAlign(CENTER, CENTER);
+        rectMode(CENTER);
+        noLoop();
     }
 
     this.draw = () => {
-        background(0, 0, 0);
-        fill(255, 255, 255);
-        textSize(100);
-        text("MENU", 300, 200);
+        translate(WIDTH / 2, 0);
+        strokeWeight(6);
+        stroke(255);
+        fill(0, 0);
+        rect(0, HEIGHT * 0.15, 600, 130);
+
+        fill(255);
+        noStroke();
+        textFont('Arial');
 
         textSize(80);
-        text("Press '1' to play", 150, 400);
+        text("It's Not Raining", 0, HEIGHT * 0.15);
 
-        textSize(80);
-        text("Press '2' for leaderboard", 175, 600, 500);
+        textSize(50);
+        text("MENU", 0, HEIGHT * 0.4);
+
+        textSize(40);
+        text("> Press '1' to play", 0, 650);
+
+        textSize(40);
+        text("> Press '2' for leaderboard", 0, 750);
     }
 
     this.keyPressed = () => {
         switch(key) {   
             case '1':
                 console.log("Game");
+                loop();
                 this.sceneManager.showScene(Game);
                 break;
             case '2':
                 console.log("Leaderboard");
+                loop();
                 this.sceneManager.showScene(Leaderboard); 
                 break;   
         }
