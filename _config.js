dd62d9d46661e25db2c76aab6994315462220ac9
Miguel Montoya <miguel.mzbi@gmail.com>
2018-02-14 20:14:08 -0600

    config file

--- _config.js
+++ _config.js
@@ -1,10 +1,7 @@
-const dotenv = require('dotenv').config();
-
 var config = {};
-
 config.mongoURI = {
-  development: "mongodb://" + process.env.MONGO_USER + ":" + process.env.MONGO_PASS + "@its-not-raining-shard-00-00-nbjkh.mongodb.net:27017,its-not-raining-shard-00-01-nbjkh.mongodb.net:27017,its-not-raining-shard-00-02-nbjkh.mongodb.net:27017/" + process.env.MONGO_DB + "?ssl=true&replicaSet=its-not-raining-shard-0&authSource=admin",
-  test: "mongodb://" + process.env.MONGO_USER + ":" + process.env.MONGO_PASS + "@its-not-raining-testing-shard-00-00-yifej.mongodb.net:27017,its-not-raining-testing-shard-00-01-yifej.mongodb.net:27017,its-not-raining-testing-shard-00-02-yifej.mongodb.net:27017/" + process.env.MONGO_DB + "?ssl=true&replicaSet=its-not-raining-testing-shard-0&authSource=admin"
+	development: "mongodb://" + process.env.MONGO_USER + ":" + process.env.MONGO_PASS + "@its-not-raining-shard-00-00-nbjkh.mongodb.net:27017,its-not-raining-shard-00-01-nbjkh.mongodb.net:27017,its-not-raining-shard-00-02-nbjkh.mongodb.net:27017/" + process.env.MONGO_DB + "?ssl=true&replicaSet=its-not-raining-shard-0&authSource=admin",
+	test: "mongodb://" + process.env.MONGO_USER + ":" + process.env.MONGO_PASS + "@its-not-raining-testing-shard-00-00-yifej.mongodb.net:27017,its-not-raining-testing-shard-00-01-yifej.mongodb.net:27017,its-not-raining-testing-shard-00-02-yifej.mongodb.net:27017/" + process.env.MONGO_DB + "?ssl=true&replicaSet=its-not-raining-testing-shard-0&authSource=admin"
 };
 
 module.exports = config;
