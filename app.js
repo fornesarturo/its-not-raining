31616ca0576a1dc665d1696164b3071be4942a06
Miguel Montoya <miguel.mzbi@gmail.com>
2018-02-16 12:29:37 -0600

    Heroku fix

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ var scoreModel = require('./models/score');
 mongoose.Promise = global.Promise;
 var envir = app.settings.env || "development";
 var dbUrl = config.mongoURI[envir] ;
+console.log(envir);
+console.log(dbUrl);
 mongoose.connect(dbUrl, (err) => {
 	if(err) {
 		console.log('Error connecting to the database: ', process.env.MONGO_DB, "\n", err);
