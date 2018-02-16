87febfdb4c00d5e1165b29772dab460884d3b1ab
Miguel Montoya <miguel.mzbi@gmail.com>
2018-02-16 11:38:07 -0600

    Commented functionality

--- databaseScripts/getScore.js
+++ databaseScripts/getScore.js
@@ -19,12 +19,24 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 db.once('open', () => {
 
-    // scoreModel.remove({userId: "andres"}, (err) => {
+    // scoreModel.remove({userId: "Andres"}, (err) => {
     //     if(err) console.log(err);
     //     else console.log("REMOVED");
     // });
+    
+    // scoreModel.update({"$or":
+    //                     [{userId: "ANDB"}, 
+    //                     {userId: "TEFY"}, 
+    //                     {userId: "JERA"}]
+    //                 }, 
+    //                 {userId: "BARRO DEJA DE HACER TRAMPA"}, 
+    //                 { multi: true },
+    //                 (err) => {
+    //                     if(err) console.log(err)
 
-    scoreModel.find({}, (err, scoresS) => {
+    // });
+    
+    scoreModel.find({userId: "BARRO DEJA DE HACER TRAMPA"}, (err, scoresS) => {
         if(err) {
             console.log(err);
             mongoose.connection.close();
